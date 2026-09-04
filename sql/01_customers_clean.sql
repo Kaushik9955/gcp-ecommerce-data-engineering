88472aef9f864b794028e183f133bd740e295e32
Purantra Kaushik <kpurantra@gmail.com>
2026-09-04 22:34:14 +0530

    Update 01_customers_clean.sql

--- sql/01_customers_clean.sql
+++ sql/01_customers_clean.sql
@@ -1,1 +1,14 @@
-
+CREATE OR REPLACE TABLE
+  `gcp-ecommerce-de-2026.ecommerce_silver.customers_clean` AS
+SELECT
+  customer_id,
+  TRIM(customer_name) AS customer_name,
+  LOWER(TRIM(email)) AS email,
+  TRIM(city) AS city,
+  TRIM(state) AS state,
+  signup_date
+FROM
+  `gcp-ecommerce-de-2026.ecommerce_bronze.customers`
+WHERE
+  customer_id IS NOT NULL
+  AND TRIM(customer_id) != '';
